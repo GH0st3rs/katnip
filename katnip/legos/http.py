b8765c8477d062cc839418d83a006cedd8156719
GH0st3rs <GH0st3rs@users.noreply.github.com>
2019-05-13 19:15:06 +0300

    Add calculate valid bit_length for Integer fields
    Add Header Field parameters by rfc2616. Example:
    Content-Type: text/html; charset=utf-8

--- katnip/legos/http.py
+++ katnip/legos/http.py
@@ -12,43 +12,68 @@ def _keyname(txt):
     return '%s_key' % txt
 
 
+def bit_length(num):
+    for x in range(1, 100):
+        if num.bit_length() > (2 << x):
+            continue
+        else:
+            return 2 << x
+
+
 class CustomHeaderField(Container):
-    def __init__(self, key, value, end=False, fuzzable_key=False, fuzzable_value=True):
+    def __init__(self, key, value, params={}, end=False, fuzzable_key=False, fuzzable_value=True, name=None):
         fields = [
             String(name=_keyname(key), value=key, fuzzable=fuzzable_key),
             Static(': '),
             Container(name=_valuename(key), fields=value, fuzzable=fuzzable_value),
-            Static('\r\n')
         ]
+        # Append Field value params, splitted by '; '
+        for k, v in params.items():
+            fields += [
+                Static('; '),
+                String(value=k, name='%s_param_%s' % (_valuename(key), k)),
+                Static('="'),
+                String(value=v, name='%s_param_%s' % (_valuename(key), v)),
+                Static('"')
+            ]
+        # Append end of line for current Field
+        fields.append(Static('\r\n'))
         if end:
             fields.append(Static('\r\n'))
-        super(CustomHeaderField, self).__init__(name=key, fields=fields, fuzzable=fuzzable_value)
+        name = key if name is None else name
+        super(CustomHeaderField, self).__init__(name=name, fields=fields, fuzzable=fuzzable_value)
 
 
 class TextField(CustomHeaderField):
-    def __init__(self, key, value, end=False, fuzzable_key=False, fuzzable_value=True):
+    def __init__(self, key, value, params={}, end=False, fuzzable_key=False, fuzzable_value=True, name=None):
         value_field = String(name="value", value=value)
-        super(TextField, self).__init__(key, value_field, end, fuzzable_key, fuzzable_value)
+        super(TextField, self).__init__(key=key, value=value_field, params=params,
+                                        end=end, fuzzable_key=fuzzable_key,
+                                        fuzzable_value=fuzzable_value, name=name)
 
 
 class IntField(CustomHeaderField):
-    def __init__(self, key, value, end=False, fuzzable_key=False, fuzzable_value=True):
+    def __init__(self, key, value, params={}, end=False, fuzzable_key=False, fuzzable_value=True, name=None):
         value_field = DecimalNumber(
             name="value",
             value=value,
-            num_bits=32,
+            num_bits=bit_length(value),
             signed=True
         )
-        super(IntField, self).__init__(key, value_field, end, fuzzable_key, fuzzable_value)
+        super(IntField, self).__init__(key=key, value=value_field, params=params,
+                                       end=end, fuzzable_key=fuzzable_key,
+                                       fuzzable_value=fuzzable_value, name=name)
 
 
 class ContentLengthField(CustomHeaderField):
     def __init__(self, value, sized_field, end=False, fuzzable_key=False, fuzzable_value=True):
         value_field = OneOf(fields=[
-            Size(sized_field=sized_field, length=32, encoder=ENC_INT_DEC),
-            DecimalNumber(name="value", value=value, num_bits=32, signed=True),
+            Size(sized_field=sized_field, length=bit_length(value), encoder=ENC_INT_DEC),
+            DecimalNumber(name="value", value=value, num_bits=bit_length(value), signed=True),
         ])
-        super(ContentLengthField, self).__init__('Content-Length', value_field, end, fuzzable_key, fuzzable_value)
+        super(ContentLengthField, self).__init__(key='Content-Length', value=value_field,
+                                                 end=end, fuzzable_key=fuzzable_key,
+                                                 fuzzable_value=fuzzable_value)
 
 
 class AuthorizationField(CustomHeaderField):
@@ -61,11 +86,13 @@ class AuthorizationField(CustomHeaderField):
                 String(name='password', value=password, fuzzable=fuzz_password),
             ], encoder=ENC_BITS_BASE64)
         ]
-        super(AuthorizationField, self).__init__(key, value_field, end, fuzzable_key, fuzzable)
+        super(AuthorizationField, self).__init__(key=key, value=value_field, params={},
+                                                 end=end, fuzzable_key=fuzzable_key,
+                                                 fuzzable_value=fuzzable)
 
 
 class HttpRequestLine(Container):
-    def __init__(self, name='http_request_line', method='GET', uri='/', protocol='HTTP', version=1.0, fuzzable_method=False, fuzzable_uri=False, fuzzable=True):
+    def __init__(self, name='http_request_line', method='GET', uri='/', protocol='HTTP', version=1.0, fuzzable_method=False, fuzzable_uri=True, fuzzable=True):
         method_value = [method] if isinstance(method, str) else method
         parsed = urlparse(uri)
         uri_value = [Path(path=parsed.path, name='path', fuzz_delims=False)]
